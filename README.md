230c3ff8646f46de833d86e9599e548bbb53eead
prograweb99 <prograweb.ejemplo@gmail.com>
2021-11-23 13:15:49 -0600

    Cambio y subida de archivos

---
+++ README.md
@@ -0,0 +1,2 @@
+# prograweb99.github.io
+Ejemplo De Conexión a la B.D con Firebase
---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyACFFi4fgX4l8X3i233myQarSLRAFY5BFU",
+  authDomain: "alumnos-af684.firebaseapp.com",
+  projectId: "alumnos-af684",
+  storageBucket: "alumnos-af684.appspot.com",
+  messagingSenderId: "487024643642",
+  appId: "1:487024643642:web:0cabf39a63b99a0e256f03",
+  measurementId: "G-S6VS74822Y"
+});
